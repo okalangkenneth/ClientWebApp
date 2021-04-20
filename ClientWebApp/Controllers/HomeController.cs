976e067036dd27aec44d961532e5834277a17ab2
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-04-20 14:08:01 +0200

    Put, Patch, GetWithStream, GetWithCompletion

--- ClientWebApp/Controllers/HomeController.cs
+++ ClientWebApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using ClientWebApp.Models;
+﻿using ClientWebApp.Extensions;
+using ClientWebApp.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MyNamespace;
@@ -69,8 +71,8 @@ namespace ClientWebApp.Controllers
             var events = JsonConvert.DeserializeObject<IEnumerable<EventDayDto>>(content);
 
             return View();
-        } 
-        
+        }
+
         //Post
         public async Task<IActionResult> Post()
         {
@@ -88,7 +90,7 @@ namespace ClientWebApp.Controllers
             };
 
             //ShortCut
-           // var request = await httpClient.PostAsync("api/events", new StringContent(JsonConvert.SerializeObject(eventDto), Encoding.UTF8, "application/json"));
+            // var request = await httpClient.PostAsync("api/events", new StringContent(JsonConvert.SerializeObject(eventDto), Encoding.UTF8, "application/json"));
 
             var jsonData = JsonConvert.SerializeObject(eventDto);
 
@@ -105,6 +107,148 @@ namespace ClientWebApp.Controllers
 
             return View();
         }
+
+        public async Task<IActionResult> Put()
+        //public async Task<IActionResult> Index()
+        {
+
+            var eventDto = new EventDayDto()
+            {
+                Name = "FromClient",
+                EventDate = new DateTime(2020, 12, 18),
+                LocationAddress = "Storgatan 12",
+                LocationCityTown = "Stockholm",
+                LocationStateProvince = "Stockholm",
+                LocationPostalCode = "12345",
+                LocationCountry = "Norge",
+                Length = 1,
+            };
+
+            var jsonData = JsonConvert.SerializeObject(eventDto);
+
+            var request = new HttpRequestMessage(HttpMethod.Put, "api/events/FromClient");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Content = new StringContent(jsonData); //, Encoding.Default, "application/json");
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var updatedEvent = JsonConvert.DeserializeObject<EventDayDto>(content);
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> PatchInsert()
+        // public async Task<IActionResult> Index()
+        {
+
+            var patchDokument = new JsonPatchDocument<EventDayDto>();
+
+            var lecture = new LectureDto
+            {
+                Title = "Patch",
+                Level = 2,
+                SpeakerId = 1
+            };
+
+
+            patchDokument.Add(e => e.Lectures, new List<LectureDto>() { lecture });
+
+            var jsonData = JsonConvert.SerializeObject(patchDokument);
+
+            var request = new HttpRequestMessage(HttpMethod.Patch, "api/events/FromClient");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Content = new StringContent(jsonData); //, Encoding.Default, "application/json");
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json-patch+json");
+
+            var response = await httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var updatedEvent = JsonConvert.DeserializeObject<EventDayDto>(content);
+
+            return View();
+        }
+
+
+        public async Task<IActionResult> Patch()
+        //public async Task<IActionResult> Index()
+        {
+
+            var patchDokument = new JsonPatchDocument<EventDayDto>();
+
+            // patchDokument.Remove(e => e.Name);
+            patchDokument.Remove(e => e.LocationPostalCode);
+            patchDokument.Replace(e => e.LocationCountry, "Danmark");
+            patchDokument.Add(e => e.Length, 5);
+
+            var jsonData = JsonConvert.SerializeObject(patchDokument);
+
+            var request = new HttpRequestMessage(HttpMethod.Patch, "api/events/FromClient");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Content = new StringContent(jsonData); //, Encoding.Default, "application/json");
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json-patch+json");
+
+            var response = await httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var updatedEvent = JsonConvert.DeserializeObject<EventDayDto>(content);
+
+            return View();
+        }
+
+        //Get with Streams
+        public async Task<IActionResult> GetWithStreams()
+       // public async Task<IActionResult> Index()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/events");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            using (var response = await httpClient.SendAsync(request))
+            {
+
+                response.EnsureSuccessStatusCode();
+
+                var stream = await response.Content.ReadAsStreamAsync();
+
+                using (var streamReader = new StreamReader(stream))
+                {
+                    using (var jsonTextReader = new JsonTextReader(streamReader))
+                    {
+                        var serializer = new JsonSerializer();
+                        var events = serializer.Deserialize<IEnumerable<EventDayDto>>(jsonTextReader);
+                    }
+                }
+            }
+
+            return View();
+        } 
+        
+       // public async Task<IActionResult> GetWithStreamsAndCompletionMode()
+        public async Task<IActionResult> Index()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/events");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            using (var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead))
+            {
+
+                response.EnsureSuccessStatusCode();
+
+                var stream = await response.Content.ReadAsStreamAsync();
+                var events = stream.ReadAndDeserializeFromJson<IEnumerable<EventDayDto>>();
+            }
+
+            return View();
+        }
+
+
+
+
 
         public IActionResult Privacy()
         {
---
+++ ClientWebApp/Extensions/StreamExtensions.cs
@@ -0,0 +1,32 @@
+﻿using MyNamespace;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientWebApp.Extensions
+{
+    public static class StreamExtensions
+    {
+        public static T ReadAndDeserializeFromJson<T>(this Stream stream)
+        {
+            if (stream is null)
+                throw new ArgumentNullException(nameof(stream));
+
+            if (!stream.CanRead)
+                throw new NotSupportedException(nameof(stream));
+
+
+            using (var streamReader = new StreamReader(stream))
+            {
+                using (var jsonTextReader = new JsonTextReader(streamReader))
+                {
+                    var serializer = new JsonSerializer();
+                    return serializer.Deserialize<T>(jsonTextReader);
+                }
+            }
+        }
+    }
+}
